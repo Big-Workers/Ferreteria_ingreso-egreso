eb1032a90a24146ce654b3f6647c18264c0f0e36
Deivid-Ruales <davidral33@hotmail.com>
2022-08-23 19:27:58 -0500

    Creación del proyecto en Intellij idea

---
+++ src/test/java/bigworkers/ingresoegreso/ferreteria/FerreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package bigworkers.ingresoegreso.ferreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
