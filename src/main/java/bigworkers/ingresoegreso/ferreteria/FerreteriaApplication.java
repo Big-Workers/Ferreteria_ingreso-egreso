d167b03352fcb91a82bc1651d2ab4924defe5134
Deivid-Ruales <davidral33@hotmail.com>
2022-08-25 21:36:57 -0500

    Cambio

--- src/main/java/bigworkers/ingresoegreso/ferreteria/FerreteriaApplication.java
+++ src/main/java/bigworkers/ingresoegreso/ferreteria/FerreteriaApplication.java
@@ -1,5 +1,6 @@
 package bigworkers.ingresoegreso.ferreteria;
 
+import bigworkers.ingresoegreso.ferreteria.entity.Enterprise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
