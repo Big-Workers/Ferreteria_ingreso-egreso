99cdec1aa3166f9ca55c8a5454efac02047f8e9c
Deivid-Ruales <davidral33@hotmail.com>
2022-08-26 14:02:19 -0500

    Creación objeto empresa de ejemplo

--- src/main/java/bigworkers/ingresoegreso/ferreteria/FerreteriaApplication.java
+++ src/main/java/bigworkers/ingresoegreso/ferreteria/FerreteriaApplication.java
@@ -1,5 +1,6 @@
 package bigworkers.ingresoegreso.ferreteria;
 
+import bigworkers.ingresoegreso.ferreteria.entity.Enterprise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FerreteriaApplication {
 
 	public static void main(String[] args) {SpringApplication.run(FerreteriaApplication.class, args);}{
+
+		//PRUEBA PARA CREAR UNA INSTANCIA DE LA CLASE ENTERPRISE
+
+		//CREAR EMPRESA USANDO EL CONSTRUCTOR DE LA CLASE
+		Enterprise empresa = new Enterprise(123456789,"Ferreteria la tuerca","12992999","Carrera 25 # 10-80","601235645");
+
+		//OBTENER LOS DATOS DE LA EMPRESA
+		System.out.println(empresa.toString());
+
+		//MODIFICAR DATOS DE LA EMPRESA CREADA
+		empresa.setName("Ferreteria tornillo");
+		empresa.setAddress("Calle 2 # 10-20");
+		empresa.setDocument("12000000");
+		empresa.setPhone("3101234566");
+
+		//OBTENER LOS NUEVOS DATOS DE LA EMPRESA
+		System.out.println(empresa.toString());
 
 	}
 
