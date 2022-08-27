c5dbbcd6eaabe464c4d8c1c609cace07db7f0e0a
oscar paternina <paternina0412@gmail.com>
2022-08-27 16:21:32 -0500

    Agregar clase Employee a la rama oscar

--- src/main/java/bigworkers/ingresoegreso/ferreteria/Employee.java
+++ src/main/java/bigworkers/ingresoegreso/ferreteria/Employee.java
@@ -7,18 +7,21 @@ public class Employee {
     private String name;
     private String surnames;
     private String address;
+    private String phoneNumber;
+
     private String emails;
     private int age;
     private String role;
     private boolean state;
 
-    public Employee(String idUser, String documentType, String documentNumber, String name, String surnames, String address, String emails, int age, String role, boolean state){
+    public Employee(String idUser, String documentType, String documentNumber, String name, String surnames, String address, String phoneNumber, String emails, int age, String role, boolean state){
         this.idUser = idUser;
         this.documentType = documentType;
         this.documentNumber = documentNumber;
         this.name = name;
         this.surnames = surnames;
         this.address = address;
+        this.phoneNumber = phoneNumber;
         this.emails = emails;
         this.age = age;
         this.role = role;
@@ -45,6 +48,9 @@ public class Employee {
 
     public String getAddress() {return address;}
     public void setAddress(String address) {this.address = address;}
+
+    public String getPhoneNumber() {return phoneNumber;}
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
 
     public String getEmails() {return emails;}
     public void setEmails(String emails) {this.emails = emails;}
