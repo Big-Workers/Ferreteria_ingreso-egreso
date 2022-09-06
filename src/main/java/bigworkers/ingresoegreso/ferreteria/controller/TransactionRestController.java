cdf32eb3fa1b0b3e2e566d68ffba009e69be7bc5
diego8324 <diegoarmandq@hotmail.com>
2022-09-06 18:06:55 -0500

    creacion del servicio Transaction y su interfase

--- src/main/java/bigworkers/ingresoegreso/ferreteria/controller/TransactionRestController.java
+++ src/main/java/bigworkers/ingresoegreso/ferreteria/controller/TransactionRestController.java
@@ -2,6 +2,8 @@ package bigworkers.ingresoegreso.ferreteria.controller;
 
 
 import bigworkers.ingresoegreso.ferreteria.entity.Transaction;
+import bigworkers.ingresoegreso.ferreteria.service.ITransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,48 +11,30 @@ import org.springframework.web.bind.annotation.*;
 
 public class TransactionRestController {
 
+    @Autowired //inyecta las interfases
+    public ITransactionService TransactionService;
+
+
+
     @GetMapping("/transaction/{id}")
     public Transaction findById(@PathVariable long id){
-        Transaction movimiento = new Transaction();
-        movimiento.setIdTransaction(id);
-        movimiento.setAmount(100000);
-        movimiento.setConcept("gastos");
-        movimiento.setDetail("compra de bombillos");
-        movimiento.setEnterprise(movimiento.getEnterprise());
-        movimiento.setEmployee(movimiento.getEmployee());
-        movimiento.setState(movimiento.isState());
-        return movimiento;
+        return TransactionService.findById(id);
 
 
     }
     @PostMapping("/transaction")
     public Transaction CreateTransaction (@RequestBody Transaction movimiento){
-        Transaction newMovimiento = new Transaction();
-        newMovimiento.setIdTransaction(1);
-        newMovimiento.setAmount(movimiento.getAmount());
-        newMovimiento.setConcept(movimiento.getConcept());
-        newMovimiento.setDetail(movimiento.getDetail());
-        newMovimiento.setEnterprise(movimiento.getEnterprise());
-        newMovimiento.setEmployee(movimiento.getEmployee());
-        newMovimiento.setState(movimiento.isState());
-        return newMovimiento;
+        return TransactionService.CreateTransaction(movimiento);
 
     }
     @PutMapping("/transaction/{id}")
     public Transaction updateTransaction(@PathVariable long id, @RequestBody Transaction movimiento){
-        Transaction putTransaction = findById(id);
-        putTransaction.setAmount(movimiento.getAmount());
-        putTransaction.setConcept(movimiento.getConcept());
-        putTransaction.setDetail(movimiento.getDetail());
-        putTransaction.setEnterprise(movimiento.getEnterprise());
-        putTransaction.setEmployee(movimiento.getEmployee());
-        putTransaction.setState(movimiento.isState());
-        return putTransaction;
+        return TransactionService.updateTransaction(id, movimiento);
 
     }
     @DeleteMapping("/transaction/{id}")
     public void deleteTransaction(@PathVariable long id){
-        Transaction putTransaction = findById(id);
+        return TransactionService.deleteTransaction(long id);
     }
 
 
--- src/main/java/bigworkers/ingresoegreso/ferreteria/service/TransactionService.java
+++ src/main/java/bigworkers/ingresoegreso/ferreteria/service/TransactionService.java
@@ -1,7 +1,52 @@
 package bigworkers.ingresoegreso.ferreteria.service;
 
+import bigworkers.ingresoegreso.ferreteria.entity.Transaction;
+
 public class TransactionService implements ITransactionService{
 
 
+    @Override
+    public Transaction findById(long id) {
+        Transaction movimiento = new Transaction();
+        movimiento.setIdTransaction(id);
+        movimiento.setAmount(100000);
+        movimiento.setConcept("gastos");
+        movimiento.setDetail("compra de bombillos");
+        movimiento.setEnterprise(movimiento.getEnterprise());
+        movimiento.setEmployee(movimiento.getEmployee());
+        movimiento.setState(movimiento.isState());
+        return null;
+    }
 
+    @Override
+    public Transaction CreateTransaction(Transaction movimiento) {
+        Transaction newMovimiento = new Transaction();
+        newMovimiento.setIdTransaction(1);
+        newMovimiento.setAmount(movimiento.getAmount());
+        newMovimiento.setConcept(movimiento.getConcept());
+        newMovimiento.setDetail(movimiento.getDetail());
+        newMovimiento.setEnterprise(movimiento.getEnterprise());
+        newMovimiento.setEmployee(movimiento.getEmployee());
+        newMovimiento.setState(movimiento.isState());
+
+        return null;
+    }
+
+    @Override
+    public Transaction updateTransaction(long id, Transaction movimiento) {
+        Transaction putTransaction = findById(id);
+        putTransaction.setAmount(movimiento.getAmount());
+        putTransaction.setConcept(movimiento.getConcept());
+        putTransaction.setDetail(movimiento.getDetail());
+        putTransaction.setEnterprise(movimiento.getEnterprise());
+        putTransaction.setEmployee(movimiento.getEmployee());
+        putTransaction.setState(movimiento.isState());
+
+        return null;
+    }
+
+    @Override
+    public void deleteTransaction(long id) {
+        Transaction deleteTransaction = findById(id);
+    }
 }
