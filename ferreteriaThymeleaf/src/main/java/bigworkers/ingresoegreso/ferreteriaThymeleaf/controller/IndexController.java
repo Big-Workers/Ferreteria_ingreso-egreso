1f0cb7cfe67ae02022658f18b6326176bcaed725
Deivid-Ruales <davidral33@hotmail.com>
2022-09-13 14:23:20 -0500

    Creación proyecto con thymeleaf

---
+++ ferreteriaThymeleaf/src/main/java/bigworkers/ingresoegreso/ferreteriaThymeleaf/controller/IndexController.java
@@ -0,0 +1,15 @@
+package bigworkers.ingresoegreso.ferreteriaThymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    private String index(){
+
+        return "index";
+    }
+}
