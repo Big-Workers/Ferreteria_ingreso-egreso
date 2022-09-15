fee0a0090e2788060ea8ea6fffaf0e1de3979421
Deivid-Ruales <davidral33@hotmail.com>
2022-09-14 19:17:18 -0500

    Código base con thymeleaf

--- ferreteriaThymeleaf/src/main/java/bigworkers/ingresoegreso/ferreteriaThymeleaf/controller/TransactionController.java
+++ ferreteriaThymeleaf/src/main/java/bigworkers/ingresoegreso/ferreteriaThymeleaf/controller/TransactionController.java
@@ -1,10 +1,12 @@
 package bigworkers.ingresoegreso.ferreteriaThymeleaf.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@Controller
 public class TransactionController {
 
     private final Logger LOG = Logger.getLogger(""+TransactionController.class);
